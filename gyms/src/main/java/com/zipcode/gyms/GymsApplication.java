935cc5b1bc843b0792fac85d7b70ddef07258c6e
b-rickert <bd.rickert52@gmail.com>
2025-12-04 18:42:15 -0500

    Initialized Spring boot with SQLite

---
+++ gyms/src/main/java/com/zipcode/gyms/GymsApplication.java
@@ -0,0 +1,13 @@
+package com.zipcode.gyms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymsApplication.class, args);
+	}
+
+}
---
+++ gyms/src/test/java/com/zipcode/gyms/GymsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zipcode.gyms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
