04e6a0306caa65e02c08c9d57d729d8c47e40af2
b-rickert <bd.rickert52@gmail.com>
2025-12-04 21:58:35 -0500

    Created gym entity

---
+++ gyms/src/main/java/com/Gym.java
@@ -0,0 +1,44 @@
+package com.zipcode.gyms.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "gym")
+@Data
+
+public class Gym {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(length = 200)
+    private String address;
+
+    @Column(length = 50)
+    private String city;
+
+    @Column(length = 2)
+    private String state;
+
+    @Column(name = "phone_number", length = 20)
+    private String phoneNumber;
+
+    @Column(name = "operating_hours", length = 50)
+    private String operatingHours;
+
+    @Column(name ="number_of_equipment")
+    private Integer numberOfEquipment;
+
+    @Column(name = "gym_type", length = 50)
+    private String gymType;
+
+    @Column(name = "monthly_rate")
+    private BigDecimal monthlyRate;
+    
+}
