0f72493f0ed75f8192dcb2adaeaf482b9135dbfc
b-rickert <bd.rickert52@gmail.com>
2025-12-05 08:53:02 -0500

    started working through controllers

---
+++ gyms/src/main/java/com/zipcode/gyms/controller/GymController.java
@@ -0,0 +1,16 @@
+package com.zipcode.gyms.controller;
+
+import com.zipcode.gyms.entity.Gym;
+import com.zipcode.gyms.repository.GymRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/gyms")
+public class GymController {
+    @Autowired
+    private GymRepository gymRepository;
+
+}
