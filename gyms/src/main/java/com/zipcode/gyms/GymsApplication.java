92fe0480564826e6c6814d35a8b325a0c257ac4f
b-rickert <bd.rickert52@gmail.com>
2025-12-06 19:36:41 -0500

    Commented out project for studying

--- gyms/src/main/java/com/zipcode/gyms/GymsApplication.java
+++ gyms/src/main/java/com/zipcode/gyms/GymsApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GymsApplication {
 
+	// main method that runs when you start the app (mvn spring-boot:run)
+    // starts the embedded Tomcat server on port 8080 and loads my controllers/repositories
 	public static void main(String[] args) {
 		SpringApplication.run(GymsApplication.class, args);
 	}
